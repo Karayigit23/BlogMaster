c6dc4b0dcf02cbf0675187537f24ffb9ceb1ee56
Karayigit23 <karayigit833@gmail.com>
2023-05-03 16:11:26 +0300

    created BlackListDelete

---
+++ BlogMaster.Core/Command/BlackList/DeleteBlackListCommand.cs
@@ -0,0 +1,25 @@
+using BlogMaster.Core.InterFaces;
+using MediatR;
+
+namespace BlogMaster.Core.Command.BlackList;
+
+public class DeleteBlackListCommand:IRequest
+{
+    public int Id { get; set; }
+}
+
+public class DeleteBlackListCommandHandler : IRequestHandler<DeleteBlackListCommand>
+{
+    private readonly IBlacklistRepository _blacklistRepository;
+
+    public DeleteBlackListCommandHandler(IBlacklistRepository blacklistRepository)
+    {
+        _blacklistRepository = blacklistRepository;
+    }
+    public async Task<Unit> Handle(DeleteBlackListCommand request, CancellationToken cancellationToken)
+    {
+        var list = await _blacklistRepository.GetBlacklistById(request.Id);
+        await _blacklistRepository.DeleteBlaclist(list);
+        return Unit.Value;
+    }
+}
